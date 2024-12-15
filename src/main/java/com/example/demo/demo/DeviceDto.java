81ff5b789d4190cb29aef8b508f0df34107d69f2
vredina <theendmyfriendssss@gmail.com>
2024-12-15 23:59:43 +0300

    pravki 3

---
+++ src/main/java/com/example/demo/demo/DeviceDto.java
@@ -0,0 +1,34 @@
+package com.example.demo.demo;
+
+public class DeviceDto {
+    private String name;
+    private String macAddress;
+    private ApplicationUser user; // Добавлено поле user
+
+    // Геттеры
+    public String getName() {
+        return name;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public ApplicationUser getUser() {
+        return user;
+    }
+
+    // Сеттеры
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    public void setUser(ApplicationUser user) {
+        this.user = user;
+    }
+}
+
